1d2f4e8a7ebb5907fb3ed77e2ec64357949c6463
ziheng <shadow1746556951@gmail.com>
2024-09-13 13:42:23 +0800

    chore: vite 配置相对路径

--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -4,8 +4,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     outDir: path.resolve('../../docs')
+    // Template for index.html
+    // index: path.resolve('../../docs/index.html') //之前是'../dist/index.html'
   }
 });
